231b2590b1eb75449ea2daa3f95870100ceee268
wendipradika <wendipradika8@gmail.com>
2020-02-06 10:03:50 +0800

    membuat halaman login

--- halaman_login/index.php
+++ halaman_login/index.php
@@ -1,23 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="design1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
 </head>
+
 <body>
     <div class="container">
         <div class="login-form">
+
             <div class="textbox">
                 <form action="">
-                    <li><input type="text" name="username" placeholder="username" required></li>
-                    <li><input type="password" name="password" placeholder="password" required></li>
+                    <h1>login</h1>
+                    <li><i class="fa fa-user"></i><input type="text" name="username" placeholder="username" required autocomplete="off"></li>
+                    <li><i class="fa fa-key"></i><input type="password" name="password" placeholder="password" required></li>
                 </form>
             </div>
-            <button class="tombol">LOGIN</button>
+            <button class="tombol">login</button>
         </div>
+        <div class="logo">
+            <h2>Wlcome to Skensa</h2>
+            <img src="gambar.png" alt="">
+        </div>
+        <div class="tulisan">
+
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus porro error distinctio alias. Officiis, laudantium labore consequuntur cumque totam aliquam error rerum veniam, et placeat molestiae, neque similique magni at!</span>
+        </div>
+
     </div>
+
 </body>
+
 </html>
