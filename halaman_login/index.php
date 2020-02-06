4cb8c50f932ad9df4f56fcbac5300ea5c84e2e98
wendipradika <wendipradika8@gmail.com>
2020-02-06 08:06:12 +0800

    membuat halaman login

---
+++ halaman_login/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="design.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="login-form">
+            <div class="textbox">
+                <form action="">
+                    <li><input type="text" name="username" placeholder="username" required></li>
+                    <li><input type="password" name="password" placeholder="password" required></li>
+                </form>
+            </div>
+            <button class="tombol">LOGIN</button>
+        </div>
+    </div>
+</body>
+</html>
